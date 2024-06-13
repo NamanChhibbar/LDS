8b263066a2d887f0c7d87f5536adcff8c66e234c
Naman Chhibbar <n.chhibar@gmail.com>
2024-06-13 14:00:59 -0600

    Removed unused code

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import re
 
-def preprocess_text(text, stop_words=None):
+def preprocess_text(text: str, stop_words: list[str]=[]):
+
+	# Remove leading and trailing white spaces
+	text = text.strip()
 
 	# Convert non-ASCII quotes to ASCII quotes
 	text = re.sub(r"‘|’", "'", text)
@@ -27,8 +30,8 @@ def preprocess_text(text, stop_words=None):
 	# Remove stop words
 	if stop_words:
 		text = re.sub(r"|".join([
-			rf"\s{word}[\s.]" for word in stop_words
-		]), " ", text)
+			rf"\s{word}([\s.!:;])" for word in stop_words
+		]), r"\1", text)
 
 	# Concatenating multiple spaces and tabs
 	text = re.sub(r"[ \t]{2,}", " ", text)
