7d6d78b317294c13b161129c376aac9a7a91fa17
NamanChhibbar <n.chhibar@gmail.com>
2024-06-28 15:28:15 -0600

    Added option to provide max_tokens in encoder calls; refactored encoders.py

--- utils/encoders.py
+++ utils/encoders.py
@@ -16,10 +16,11 @@ class Encoder(ABC):
 	Base class for encoders
 	"""
 	def __init__(
-			self, tokenizer, preprocessor: TextProcessor|None=None,
-			add_special_tokens: bool=True, bos_id: int|None=None,
-			eos_id: int|None=None
-		) -> None:
+		self, tokenizer, max_tokens: int,
+		preprocessor: TextProcessor|None=None,
+		add_special_tokens: bool=True, bos_id: int|None=None,
+		eos_id: int|None=None
+	) -> None:
 		"""
 		## Parameters
 		`tokenizer`: Hugging Face tokenizer
@@ -27,12 +28,15 @@ class Encoder(ABC):
 		"""
 		super().__init__()
 		self.tokenizer = tokenizer
+		self.max_tokens = max_tokens
 		self.preprocessor = preprocessor
 		self.add_special_tokens = add_special_tokens
 		self.bos_id = bos_id
 		self.eos_id = eos_id
 
-	def __call__(self, texts: str|list[str]) -> BatchEncoding:
+	def __call__(
+		self, texts: str|list[str], max_tokens: int|None=None
+	) -> BatchEncoding:
 		"""
 		Encode texts
 
@@ -44,13 +48,17 @@ class Encoder(ABC):
 		"""
 		if isinstance(texts, str):
 			texts = [texts]
+		if max_tokens is None:
+			max_tokens = self.max_tokens
 		if self.preprocessor is not None:
 			texts = self.preprocessor(texts)
-		encodings = self.generate_encodings(texts)
+		encodings = self.generate_encodings(texts, max_tokens)
 		return encodings
 	
 	@abstractmethod
-	def generate_encodings(self, texts: list[str]) -> BatchEncoding:
+	def generate_encodings(
+		self, texts: list[str], max_tokens: int
+	) -> BatchEncoding:
 		...
 	
 	def add_tokens(self, encodings: list[int]):
@@ -67,21 +75,26 @@ class Encoder(ABC):
 class TruncateMiddle(Encoder):
 
 	def __init__(
-			self, tokenizer, context_size:int, head_size: float=.5,
-			preprocessor: TextProcessor|None=None, add_special_tokens: bool=True
-		) -> None:
+		self, tokenizer, max_tokens: int,
+		head_size: float=.5,
+		preprocessor: TextProcessor|None=None,
+		add_special_tokens: bool=True
+	) -> None:
 		super().__init__(
-			tokenizer, preprocessor, add_special_tokens,
+			tokenizer, max_tokens, preprocessor, add_special_tokens,
 			tokenizer.bos_token_id, tokenizer.eos_token_id
 		)
-		self.context_size = context_size
 		self.head_size = head_size
 
-	def generate_encodings(self, texts: list[str]) -> BatchEncoding:
-		context_size = self.context_size - 2
+	def generate_encodings(
+		self, texts: list[str], max_tokens: int
+	) -> BatchEncoding:
 		tokenizer = self.tokenizer
+		if self.add_special_tokens:
+			max_tokens -= 2
+
 		# Constant head size
-		head_size = int(context_size * self.head_size)
+		head_size = int(max_tokens * self.head_size)
 		truncated_ids = []
 
 		for text in texts:
@@ -91,9 +104,9 @@ class TruncateMiddle(Encoder):
 			)
 
 			# If the encodings dont fit in the model
-			if len(encodings) > context_size:
+			if len(encodings) > max_tokens:
 				# Calculate beginning index of tail
-				tail_idx = len(encodings) - context_size + head_size
+				tail_idx = len(encodings) - max_tokens + head_size
 
 				# Truncate the middle and concatenate head and tail
 				encodings = np.concatenate([
@@ -117,15 +130,14 @@ class TruncateMiddle(Encoder):
 class UniformSampler(Encoder):
 
 	def __init__(
-			self, tokenizer, context_size: int,
-			sent_tokenizer, preprocessor: TextProcessor|None=None,
-			add_special_tokens: bool=True, seed: int|None=None
-		) -> None:
+		self, tokenizer, max_tokens: int,
+		sent_tokenizer, preprocessor: TextProcessor|None=None,
+		add_special_tokens: bool=True, seed: int|None=None
+	) -> None:
 		super().__init__(
-			tokenizer, preprocessor, add_special_tokens,
+			tokenizer, max_tokens, preprocessor, add_special_tokens,
 			tokenizer.bos_token_id, tokenizer.eos_token_id
 		)
-		self.context_size = context_size
 		self.sent_tokenizer = sent_tokenizer
 		self.seed = seed
 		np.random.seed(seed)
@@ -133,9 +145,12 @@ class UniformSampler(Encoder):
 			SENT_SEP, add_special_tokens=False
 		)[0]
 
-	def generate_encodings(self, texts: list[str]) -> BatchEncoding:
+	def generate_encodings(
+		self, texts: list[str], max_tokens: int
+	) -> BatchEncoding:
 		tokenizer = self.tokenizer
-		context_size = self.context_size - 2
+		if self.add_special_tokens:
+			max_tokens -= 2
 
 		processed_texts = []
 		for text in texts:
@@ -143,7 +158,7 @@ class UniformSampler(Encoder):
 			encodings = tokenizer.encode(
 				text, add_special_tokens=False
 			)
-			if len(encodings) <= context_size:
+			if len(encodings) <= max_tokens:
 				processed_texts.append(encodings)
 				continue
 
@@ -160,7 +175,7 @@ class UniformSampler(Encoder):
 			])
 
 			# Approximate probability of picking a sentence
-			p = context_size / total_length
+			p = max_tokens / total_length
 
 			# Sample until sentences fit in model
 			while True:
@@ -173,7 +188,7 @@ class UniformSampler(Encoder):
 					for elm in lis + [self.sent_sep_id]
 				]
 
-				if len(sampled) <= context_size:
+				if len(sampled) <= max_tokens:
 					break
 
 			# Remove last sentence separator token
@@ -197,16 +212,15 @@ class UniformSampler(Encoder):
 class SentenceSampler(Encoder):
 
 	def __init__(
-			self, tokenizer, context_size: int, sent_tokenizer,
+			self, tokenizer, max_tokens: int, sent_tokenizer,
 			sent_encoder, preprocessor: TextProcessor|None=None,
 			add_special_tokens: bool=True, threshold: float=.7,
 			device: str|torch.device|None=None, seed: int|None=None
 		) -> None:
 		super().__init__(
-			tokenizer, preprocessor, add_special_tokens,
+			tokenizer, max_tokens, preprocessor, add_special_tokens,
 			tokenizer.bos_token_id, tokenizer.eos_token_id
 		)
-		self.context_size = context_size
 		self.sent_tokenizer = sent_tokenizer
 		self.sent_encoder = sent_encoder.to(device)
 		self.sent_embedding_dim = sent_encoder.get_sentence_embedding_dimension()
@@ -218,10 +232,13 @@ class SentenceSampler(Encoder):
 			SENT_SEP, add_special_tokens=False
 		)[0]
 
-	def generate_encodings(self, texts: list[str]) -> BatchEncoding:
+	def generate_encodings(
+		self, texts: list[str], max_tokens: int
+	) -> BatchEncoding:
 		sent_tokenizer = self.sent_tokenizer
 		tokenizer = self.tokenizer
-		context_size = self.context_size - 2
+		if self.add_special_tokens:
+			max_tokens -= 2
 
 		processed_texts = []
 		for text in texts:
@@ -229,7 +246,7 @@ class SentenceSampler(Encoder):
 			encodings = tokenizer.encode(
 				text, add_special_tokens=False
 			)
-			if len(encodings) <= context_size:
+			if len(encodings) <= max_tokens:
 				processed_texts.append(encodings)
 				continue
 
@@ -245,7 +262,7 @@ class SentenceSampler(Encoder):
 			])
 
 			# Approximate probability of picking a sentence
-			p = context_size / total_length
+			p = max_tokens / total_length
 
 			# Sample until sentences fit in model
 			while True:
@@ -268,7 +285,7 @@ class SentenceSampler(Encoder):
 						(num_sampled * sampled_embedding + sent_embedding) /
 						(num_sampled := num_sampled + 1)
 					)
-				if len(sampled) <= context_size:
+				if len(sampled) <= max_tokens:
 					break
 
 			# Remove last sentence separator token
@@ -292,16 +309,15 @@ class SentenceSampler(Encoder):
 class RemoveRedundancy(Encoder):
 
 	def __init__(
-			self, tokenizer, context_size: int, sent_tokenizer,
+			self, tokenizer, max_tokens: int, sent_tokenizer,
 			sent_encoder, preprocessor: TextProcessor|None=None,
 			add_special_tokens: bool=True, threshold: float=.7,
 			device: str|torch.device|None=None, seed: int|None=None
 		) -> None:
 		super().__init__(
-			tokenizer, preprocessor, add_special_tokens,
+			tokenizer, max_tokens, preprocessor, add_special_tokens,
 			tokenizer.bos_token_id, tokenizer.eos_token_id
 		)
-		self.context_size = context_size
 		self.sent_tokenizer = sent_tokenizer
 		self.sent_encoder = sent_encoder.to(device)
 		self.sent_embedding_dim = sent_encoder.get_sentence_embedding_dimension()
@@ -313,9 +329,12 @@ class RemoveRedundancy(Encoder):
 			SENT_SEP, add_special_tokens=False
 		)[0]
 
-	def generate_encodings(self, texts: list[str]) -> BatchEncoding:
+	def generate_encodings(
+		self, texts: list[str], max_tokens: int
+	) -> BatchEncoding:
 		tokenizer = self.tokenizer
-		context_size = self.context_size - 2
+		if self.add_special_tokens:
+			max_tokens -= 2
 
 		processed_texts = []
 		for text in texts:
@@ -323,7 +342,7 @@ class RemoveRedundancy(Encoder):
 			encodings = tokenizer.encode(
 				text, add_special_tokens=False
 			)
-			if len(encodings) <= context_size:
+			if len(encodings) <= max_tokens:
 				processed_texts.append(encodings)
 				continue
 
@@ -345,7 +364,7 @@ class RemoveRedundancy(Encoder):
 			])
 
 			# Approximate probability of picking a sentence
-			p = context_size / total_length
+			p = max_tokens / total_length
 
 			# Sample until sentences fit in model
 			while True:
@@ -359,7 +378,7 @@ class RemoveRedundancy(Encoder):
 					for elm in lis + [self.sent_sep_id]
 				]
 
-				if len(sampled) <= context_size:
+				if len(sampled) <= max_tokens:
 					break
 
 			# Remove last sentence separator token
